07b0b65c833be5c1ad32e745b3c64815b4c439a3
khalid hassan <abuuanas30@hotmail.com>
2017-09-17 20:57:41 +0300

    Modified the the the backend javascript

--- js/git.js
+++ js/git.js
@@ -1,34 +1,29 @@
 var apiKey = require('./../.env').apiKey;
+Search = function () {
+}
+Search.prototype.getUser = function() {
+var users = $('#userName').val();
+    $.get('https://api.github.com/users/' + users + '?access_token=' + apiKey).then(function(response){
+        $('.name').text(response.login);
+        $('.link').append(response.html_url);
 
-function GitHubSearch() {
-
-};
-
-
-GitHubSearch.prototype.userLookup = function(username, displayResults, displayErrorMessage){
-
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-  displayResults(username, response.followers, response.following, response.avatar_url, response.location, response.html_url, response.public_repos, response.bio, response.blog, response.company, response.email);
-  }).fail(function(error){
-  displayErrorMessage(username);
+        console.log(response)
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+    $.get('https://api.github.com/users/' + users + '/repos?access_token=' + apiKey).then(function(response){
+      console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        if (response[i].description === null) {
+          response[i].description = "No description"
+        } else {
+          response[i].description = response[i].description
+        }
+        $('.link').append("<li class = 'remove'>" +  response[i].name + "</li>" + "<p class = 'remove'>" + response[i].description + "<br>" + response[i].html_url + "</p>")
+      }
+  }).fail(function(error) {
+    console.log("errorname");
   });
-};
 
-GitHubSearch.prototype.reposLookup = function(username, displayRepos){
-
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  displayRepos(response);
-  }).fail(function(error){
-  });
-};
-
-GitHubSearch.prototype.reposbyLanguage = function(username, getReposByLanguage, language){
-
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-  getReposByLanguage(response, language);
-  }).fail(function(error){
-  });
-};
-
-exports.gitHubModule = GitHubSearch;
+  };
+exports.searchModule = Search;
