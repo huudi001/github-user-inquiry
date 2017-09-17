4e3ec54896f64339829f5f1a156a461a2cb70281
khalid hassan <abuuanas30@hotmail.com>
2017-09-17 20:58:17 +0300

    Modified the gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
-var buildProduction = utilities.env.production;
-
+var utilities = require('gulp-util');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 var lib = require('bower-files')({
   "overrides": {
     "bootstrap": {
@@ -19,30 +17,25 @@ var lib = require('bower-files')({
     }
   }
 });
-
 var browserSync = require('browser-sync').create();
-//var sourcemaps = require('gulp-sourcemaps');
-
-// Test gulp task in the terminal
-gulp.task('myTask', function() {
-  console.log("Hello Gulp");
+var buildProduction = utilities.env.production;
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
-
-// Task for jshint
-gulp.task('jshint', function() {
-  return gulp.src(['js/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+gulp.task('bowerCSS', function() {
+  return gulp.src(lib.ext('css/*css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });
-
-// Task for concat
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
-
-// Task for browserify
 gulp.task('jsBrowserify', ['concatInterface'], function() {
   return browserify({
       entries: ['./tmp/allConcat.js']
@@ -51,81 +44,45 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
 });
-
-// Task for minification
-gulp.task('minifyScripts', ['jsBrowserify'], function() {
-  return gulp.src('./build/js/app.js')
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest("./build/js"));
 });
-
-// Task for js bower_components
-gulp.task('jsBower', function() {
-  return gulp.src(lib.ext('js').files)
-    .pipe(concat('vendor.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
-});
-
-// Task for css bower_components
-gulp.task('cssBower', function() {
-  return gulp.src(lib.ext('css').files)
-    .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('./build/css'));
-});
-
-// Task bower to carry out task jsBower and cssBower
-gulp.task('bower', ['jsBower', 'cssBower']);
-
-// Task for cleaning
-gulp.task('clean', function() {
+gulp.task("clean", function() {
   return del(['build', 'tmp']);
 });
-
-// Task for creating environments
-gulp.task('build', ['clean'], function() {
+gulp.task("build", ['clean'], function() {
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
-  gulp.start('cssBuild');
 });
-
-// Task for server
-gulp.task('server', ['build'], function() {
+gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: './',
-      index: 'index.html'
+      baseDir: "./",
+      index: "index.html"
     }
   });
-
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
-  gulp.watch(['*.html'], ['htmlBuild']);
-  gulp.watch(['scss/*.scss'], ['cssBuild']);
-});
+    gulp.watch(['*.html'], ['htmlBuild']);
 
-gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
+});
+gulp.task('jshint', function() {
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
 });
-
-gulp.task('bowerBuild', ['bower'], function() {
+gulp.task('bowerBuild', ['bower'], function(){
   browserSync.reload();
 });
-
 gulp.task('htmlBuild', function() {
   browserSync.reload();
-});
-
-// Task for sass. Converts sass to css
-gulp.task('cssBuild', function() {
-  return gulp.src(['scss/*.scss'])
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.stream());
 });
