91d1225f9cf3740d37e737705c8b4f39bc868b44
khalid hassan <abuuanas30@hotmail.com>
2017-09-19 10:02:48 +0300

    changed gitignore

---
+++ tmp/allConcat.js
@@ -0,0 +1,12 @@
+var Search = require('./../js/git.js').searchModule;
+$(document).ready(function() {
+  var nameSearch = new Search();
+  $('#searchName').click(function() {
+    var users = $('#userName').val();
+    var x = nameSearch.getUser(users)
+    $('.result').show()
+    $("li.remove").remove();
+    $("p.remove").remove();
+
+  })
+})
